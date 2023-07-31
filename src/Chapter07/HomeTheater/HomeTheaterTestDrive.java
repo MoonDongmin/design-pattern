9a094066086ed590c7238cc9cd433b5595dfd151
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-07-31 10:28:12 +0900

    study Chap07

--- src/Chapter07/HomeTheater/HomeTheaterTestDrive.java
+++ src/Chapter07/HomeTheater/HomeTheaterTestDrive.java
@@ -4,11 +4,11 @@ public class HomeTheaterTestDrive {
     public static void main(String[] args) {
         //구성 요소 초기화
 
-        HomeTheaterFacade homeTheater =
-                new HomeTheaterFacade(amp, tuner, player,
-                        projector, screen, lights, popper);
+//        HomeTheaterFacade homeTheater =
+//                new HomeTheaterFacade(amp, tuner, player,
+//                        projector, screen, lights, popper);
 
-        homeTheater.watchMovie("범죄도시3");
-        homeTheater.endMovie();
+//        homeTheater.watchMovie("범죄도시3");
+//        homeTheater.endMovie();
     }
 }
